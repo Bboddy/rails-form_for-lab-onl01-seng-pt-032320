20a781fe727d475f94084c183744371205e9930f
Bryce B <bryce.boddy@gmail.com>
2020-08-20 01:40:01 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   resources :students, only: [:index, :show, :new, :create, :edit, :update]
+=======
+  resources :students, except: [:destroy, :index]
+>>>>>>> a9cf7c984802d84ff5973846bb63ac5246e5a856
   resources :school_classes, except: [:destroy, :index]
 end
